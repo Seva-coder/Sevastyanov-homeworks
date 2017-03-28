7eccc49197a1a6050ff8a3dc9e1ee625f68a2ad7
Seva-coder <nikita.sevast@gmail.com>
2017-03-28 18:54:38 +0300

    errors disabled4

--- dz1/task_class.py
+++ dz1/task_class.py
@@ -19,7 +19,7 @@ class Task:
     state = property()
 
     @property
-    def state(self):
+    def state(self):  # pylint: disable=E0102
         return self._state
 
     @state.setter
@@ -32,7 +32,7 @@ class Task:
     is_failed = property()
 
     @property
-    def is_failed(self):
+    def is_failed(self):  # pylint: disable=E0102
         return True if self._state == 'in_progress' and self.estimate < datetime.now() else False
 
     @property
